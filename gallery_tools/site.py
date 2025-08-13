e70084dcc0ba17f6f36312988358a5f6e5976998
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2025-08-13 13:04:52 +0300

    Добавлены переменные шаблона

--- gallery_tools/site.py
+++ gallery_tools/site.py
@@ -6,11 +6,19 @@ from gallery_tools.config import TEMPLATES_DIR
 
 
 def build_site(data_json_path, template_html_path, output_html_path):
-    # 1. Загружаем список изображений
+    # Загружаем список изображений
     with open(data_json_path, "r", encoding="utf-8") as f:
         items = json.load(f)
 
-    # 2. Генерируем HTML-элементы галереи
+    # Загружаем переменные шаблона
+    vars_path = os.path.join(TEMPLATES_DIR, "vars.json")
+    if os.path.exists(vars_path):
+        with open(vars_path, "r", encoding="utf-8") as f:
+            vars_data = json.load(f)
+    else:
+        vars_data = {}
+
+    # Генерируем HTML-элементы галереи
     gallery_items_html = "\n".join(
         f'<img src="{item["msrc"]}" '
         f'data-full="{item["src"]}" '
@@ -18,29 +26,28 @@ def build_site(data_json_path, template_html_path, output_html_path):
         for item in items
     )
 
-    # 3. Читаем шаблон
+    # Читаем шаблон
     with open(template_html_path, "r", encoding="utf-8") as f:
         template_html = f.read()
 
-    # 4. Подставляем данные
+    # Подставляем переменные
+    for key, value in vars_data.items():
+        template_html = template_html.replace(f"{{{{{key}}}}}", value)
+
+    # Подставляем галерею
     result_html = template_html.replace("{{GALLERY_ITEMS}}", gallery_items_html)
 
-    # 5. Создаём папки и копируем css и js
+    # Копируем css и js
     output_dir = os.path.dirname(output_html_path)
-    css_src = os.path.join(TEMPLATES_DIR, "css")
-    js_src = os.path.join(TEMPLATES_DIR, "js")
-    css_dst = os.path.join(output_dir, "css")
-    js_dst = os.path.join(output_dir, "js")
+    for folder in ["css", "js"]:
+        src = os.path.join(TEMPLATES_DIR, folder)
+        dst = os.path.join(output_dir, folder)
+        if os.path.exists(dst):
+            shutil.rmtree(dst)
+        if os.path.exists(src):
+            shutil.copytree(src, dst)
 
-    if os.path.exists(css_dst):
-        shutil.rmtree(css_dst)
-    shutil.copytree(css_src, css_dst)
-
-    if os.path.exists(js_dst):
-        shutil.rmtree(js_dst)
-    shutil.copytree(js_src, js_dst)
-
-    # 6. Записываем итоговый HTML
+    # Сохраняем HTML
     with open(output_html_path, "w", encoding="utf-8") as f:
         f.write(result_html)
 
