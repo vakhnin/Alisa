b7c1152d516e77bcfe37297f16582f9536cab3be
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2025-08-14 08:24:54 +0300

    Добавлен контейнер

--- templates/js/gallery.js
+++ templates/js/gallery.js
@@ -1,24 +1,15 @@
 const images = document.querySelectorAll('.gallery img');
-const viewer = document.createElement('div');
-viewer.id = 'viewer';
-viewer.innerHTML = '<img><div class="caption"></div>';
-document.body.appendChild(viewer);
-
+const viewer = document.querySelector('#viewer');
 const viewerImg = viewer.querySelector('img');
 const viewerCaption = viewer.querySelector('.caption');
 
 function openImage(img) {
-    // Убираем выделение со всех
     images.forEach(i => i.classList.remove('active'));
-
-    // Добавляем выделение текущей
     img.classList.add('active');
 
-    // Меняем изображение в просмотрщике
     viewerImg.src = img.dataset.full;
     viewerCaption.textContent = img.dataset.desc || '';
 
-    // Скроллим к просмотровому блоку
     viewer.scrollIntoView({ behavior: 'smooth' });
 }
 
@@ -26,7 +17,6 @@ images.forEach((img) => {
     img.addEventListener('click', () => openImage(img));
 });
 
-// Открыть первую картинку при загрузке страницы
 if (images.length > 0) {
     openImage(images[0]);
 }
