b7706510630c1b480dbb389cf42f37e33823f00f
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2025-08-14 07:51:57 +0300

    Добавлены описания изображений

--- gallery_tools/site.py
+++ gallery_tools/site.py
@@ -6,11 +6,11 @@ from gallery_tools.config import TEMPLATES_DIR
 
 
 def build_site(data_json_path, template_html_path, output_html_path):
-    # Загружаем список изображений
+    # 1. Загружаем список изображений
     with open(data_json_path, "r", encoding="utf-8") as f:
         items = json.load(f)
 
-    # Загружаем переменные шаблона
+    # 2. Загружаем переменные шаблона
     vars_path = os.path.join(TEMPLATES_DIR, "vars.json")
     if os.path.exists(vars_path):
         with open(vars_path, "r", encoding="utf-8") as f:
@@ -18,26 +18,29 @@ def build_site(data_json_path, template_html_path, output_html_path):
     else:
         vars_data = {}
 
-    # Генерируем HTML-элементы галереи
+    # 3. Генерируем HTML-элементы галереи с description
     gallery_items_html = "\n".join(
-        f'<img src="{item["msrc"]}" '
-        f'data-full="{item["src"]}" '
-        f'data-desc="{item.get("description", "")}">'
+        f'<div class="gallery-item">'
+        f'  <img src="{item["msrc"]}" '
+        f'       data-full="{item["src"]}" '
+        f'       data-desc="{item.get("description", "")}">'
+        f'  <div class="thumb-caption">{item.get("description", "")}</div>'
+        f'</div>'
         for item in items
     )
 
-    # Читаем шаблон
+    # 4. Читаем HTML-шаблон
     with open(template_html_path, "r", encoding="utf-8") as f:
         template_html = f.read()
 
-    # Подставляем переменные
+    # 5. Подставляем переменные
     for key, value in vars_data.items():
         template_html = template_html.replace(f"{{{{{key}}}}}", value)
 
-    # Подставляем галерею
+    # 6. Подставляем галерею
     result_html = template_html.replace("{{GALLERY_ITEMS}}", gallery_items_html)
 
-    # Копируем css и js
+    # 7. Копируем css и js в выходную папку
     output_dir = os.path.dirname(output_html_path)
     for folder in ["css", "js"]:
         src = os.path.join(TEMPLATES_DIR, folder)
@@ -47,7 +50,7 @@ def build_site(data_json_path, template_html_path, output_html_path):
         if os.path.exists(src):
             shutil.copytree(src, dst)
 
-    # Сохраняем HTML
+    # 8. Записываем готовый HTML
     with open(output_html_path, "w", encoding="utf-8") as f:
         f.write(result_html)
 
